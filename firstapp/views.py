d40d92a4c0cb5ca8346ab0bc05ecf6f6344d16ec
Egor.Murashov@urfu.me <Egor.Murashov@urfu.me>
2022-01-07 01:13:22 +0500

    добавление страниц и обработка запросов

--- firstapp/views.py
+++ firstapp/views.py
@@ -4,4 +4,25 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('hello world! my first project on django')
+    return HttpResponse('<h2>Главная</h2>')
+
+
+def about(request):
+    return HttpResponse('<h2>О сайте</h2>')
+
+
+def contact(request):
+    return HttpResponse('<h2>контакты</h2>')
+
+
+def products(request, productid):
+    category = request.GET.get('cat', '')
+    output = '<h2>Продукт № {0} Категория: {1}</h2>'.format(productid, category)
+    return HttpResponse(output)
+
+
+def users(request):
+    id = request.GET.get('id', 1)
+    name = request.GET.get('name', 'мася')
+    output = '<h2>Пользователь</h2><h3>id: {0} Имя:{1}</h3>'.format(id, name)
+    return HttpResponse(output)
--- hello/urls.py
+++ hello/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import re_path
 from firstapp import views
 
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('admin/', admin.site.urls),
+    path('', views.index),
+    re_path(r'^about', views.about),
+    re_path(r'^contact', views.contact),
+    path('products/<int:productid>/', views.products),
+    path('users/', views.users),    # по умолчанию
 ]
